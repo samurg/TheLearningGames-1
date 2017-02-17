bab8790c16a932374dc712f103f783a27af7a9db
Asel Acebedo Romalde <aselromalde@gmail.com>
2017-02-17 08:11:40 +0100

    Evitado que se abra el modal de attendance si la clase esta vacia de alumnos

--- myapp/www/js/controllers.js
+++ myapp/www/js/controllers.js
@@ -737,9 +737,10 @@ function ($scope, $stateParams, $ionicModal, $http, Backand, $cookies, $state, N
 
   $scope.showAttendanceModal = function(){
     $scope.classroomName = $cookies.get('classroomName');
-    //$scope.getStudentsAttendance();                                         REVISAR AQUI
     $scope.getStudents();
-    $scope.attendanceModal.show(); 
+    if ($scope.students.length > 0) {
+      $scope.attendanceModal.show();
+    }
     $scope.date = Date.now(); 
   }
     
